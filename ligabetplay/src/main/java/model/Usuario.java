57adfbb4731b0074d2c1caaf977d1a498e4a2598
sandoval <sandovalbgasantiago@gmail.com>
2024-06-26 00:06:20 +0200

    creacion primeras clases

---
+++ ligabetplay/src/main/java/model/Usuario.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Usuario {
+    int id;
+    String nombre;
+    String email;
+    String password;
+    int idRol;
+    public Usuario() {
+    }
+    public Usuario(int id, String nombre, String email, String password, int idRol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.idRol = idRol;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getIdRol() {
+        return idRol;
+    }
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+}
